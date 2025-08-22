70fef5258fe915ee174c678072e164a5dedfea25
Shohan <shohanislamjoy@gmail.com>
2025-08-23 00:12:29 +0600

    added more credentials for test perposess.

--- lib/config/blockchain_config.dart
+++ lib/config/blockchain_config.dart
@@ -1,6 +1,31 @@
+// class BlockchainConfig {
+//   // Infura Project Details
+//   static const String infuraProjectId = 'a5d9f7740993427e96a0277723ec7c44';
+//   static const String projectName =
+//       'My First Key'; // Optional, just for reference
+
+//   // Sepolia testnet configuration
+//   static const String rpcUrl = 'https://sepolia.infura.io/v3/$infuraProjectId';
+//   static const int chainId = 11155111; // Sepolia chain ID
+
+//   // Contract details (replace with your actual deployed contract address)
+//   static const String contractAddress =
+//       '0x0629904487d908D5C5fCe3B89C8234fcf4d78DAE';
+
+//   // Developer private key (⚠️ Only for testing! Never expose real wallet keys)
+//   static const String privateKey =
+//       'ad6201382b7bf714e26b2f951a9038bc6a1eff243014223d457509f71be67ac3';
+
+//   // Example user wallet address (replace with one from your privateKey)
+//   static const String walletAddress =
+//       '0x9EBA0526580292dF4e1C50e19AEB3ec69e12E270';
+// }
+
+//office laptop credentials
+
 class BlockchainConfig {
   // Infura Project Details
-  static const String infuraProjectId = 'a5d9f7740993427e96a0277723ec7c44';
+  static const String infuraProjectId = 'bc4ac4847d624b2ea23a2d1b02eb6efe';
   static const String projectName =
       'My First Key'; // Optional, just for reference
 
@@ -14,11 +39,11 @@ class BlockchainConfig {
 
   // Developer private key (⚠️ Only for testing! Never expose real wallet keys)
   static const String privateKey =
-      'ad6201382b7bf714e26b2f951a9038bc6a1eff243014223d457509f71be67ac3';
+      '2abce993632d07da38f06dccfd5574656a358e41ddb295dd068ee478f729cfa3';
 
   // Example user wallet address (replace with one from your privateKey)
   static const String walletAddress =
-      '0x9EBA0526580292dF4e1C50e19AEB3ec69e12E270';
+      '0x566258671Fe2c24f8966b822700710cC3b7227d3';
 }
 
 class UserData {
@@ -26,7 +51,7 @@ class UserData {
   // static const String name = 'Mr.karim Uddin';
   static const String name = 'Mr.Hamim Alam';
   static const String profession = 'junior Software Engineer';
-  static final BigInt accountBalance = BigInt.from(508088);
+  static final BigInt accountBalance = BigInt.from(506988);
   static final BigInt totalTransactions = BigInt.from(1505200);
   static final BigInt onTimePayments = BigInt.from(
     45,
